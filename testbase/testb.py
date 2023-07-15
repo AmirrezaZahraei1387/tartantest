461be2a01f62fc3990ec1be7560b5d1f61609d3e
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-14 21:55:49 -0600

    the TestB.checkClass is updated to also check if the subclass have 1 parameter self or no

--- testbase/testb.py
+++ testbase/testb.py
@@ -14,22 +14,30 @@ import errors
 
 class TestB:
 
-    def checkClass(self):
+    @staticmethod
+    def checkClass():
         """this method will check if the subclass meet the requirements
         or no.
         1 - one checking if one exist or no
         2 - checking if its constructor has exactly 1 parameter"""
 
         className = TestB.__subclasses__()  # here we are getting the name of the subclass
-        if len(className) == 1:  # making sure there is exactly one subclass
+        if len(className) is 1:  # making sure there is exactly one subclass
             className = className[0]
         else:
             raise errors.NotOneSubClassError("expected exactly one subclass got "+str(className))
 
+        parameters = inspect.signature(className).parameters
+        if len(parameters) is not 1:    # check if the __init__ have more than or less than 1 parameter
+            raise errors.TooManyParametersError("expected one parameter got "+str(parameters))
+
         return className
 
-
-    def getAllMethodNames(self):
+    @staticmethod
+    def getAllMethodNames(className):
         """this method will get all the method names of the
-        subclass"""
+        subclass given as className"""
+        inspect.getmembers()
+
+
 
