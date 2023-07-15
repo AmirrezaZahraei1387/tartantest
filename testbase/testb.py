84d59d67bcff970162a190261ee6103df1223491
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-14 22:14:35 -0600

    the method TestB.getAllMethodNames is created and defined. also th constant STARTING_NAME is created

--- testbase/testb.py
+++ testbase/testb.py
@@ -11,6 +11,10 @@ import time
 import inspect
 import errors
 
+# this is the constants that defines the name that must com first in the method name of subclass that
+# contain tests
+STARTING_NAME = "testb"
+
 
 class TestB:
 
@@ -22,14 +26,14 @@ class TestB:
         2 - checking if its constructor has exactly 1 parameter"""
 
         className = TestB.__subclasses__()  # here we are getting the name of the subclass
-        if len(className) is 1:  # making sure there is exactly one subclass
+        if len(className) == 1:  # making sure there is exactly one subclass
             className = className[0]
         else:
-            raise errors.NotOneSubClassError("expected exactly one subclass got "+str(className))
+            raise errors.NotOneSubClassError("expected exactly one subclass got " + str(className))
 
         parameters = inspect.signature(className).parameters
-        if len(parameters) is not 1:    # check if the __init__ have more than or less than 1 parameter
-            raise errors.TooManyParametersError("expected one parameter got "+str(parameters))
+        if len(parameters) != 1:  # check if the __init__ have more than or less than 1 parameter
+            raise errors.TooManyParametersError("expected one parameter got " + str(parameters))
 
         return className
 
@@ -37,7 +41,25 @@ class TestB:
     def getAllMethodNames(className):
         """this method will get all the method names of the
         subclass given as className"""
-        inspect.getmembers()
+
+        obj = className()
+        methods = inspect.getmembers(object=obj, predicate=inspect.ismethod)
+
+        for method in methods:
+            if not method[0].startswith(STARTING_NAME):    # check if the method name starts with The value in
+                # STARTING_NAME or no
+                methods.remove(method)
+
+            parameterNumber = len(inspect.signature(method[1]).parameters)
+            if parameterNumber != 1:
+                raise errors.TooManyParametersError("expected one parameter for "+method[0]+" got "+method[1])
+
+        return methods
+
+
+
+
+
 
 
 
