af9683633eba6e1eae4679a0e1773b7cebc9d2da
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-15 12:18:09 -0600

    the testall.py is added and some doc strings provided

---
+++ testbase/testall.py
@@ -0,0 +1,11 @@
+"""sometimes you have a lot of tests, and you want to run all of
+them together instead running each test file individually, for
+example you have 10 python files containing unittest, and
+you want to run all of them. in that case it can help
+you do the same thing that you want.
+
+to do this you have two options
+1 - giving the path of each test file manually
+2 - giving the path of directory containing all the tests"""
+
+
