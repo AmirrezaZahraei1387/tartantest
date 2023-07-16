20fc1f04f88aced47cfa5cdf1bccd38e10432032
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-07-16 16:31:17 -0600

    the new error named FailAssertionError is now created inside the errors.py

--- asserts/errors.py
+++ asserts/errors.py
@@ -1,4 +1,26 @@
 
 
 class FailAssertionError(BaseException):
-    pass
+    """this error will be raised when the assertion went wrong
+    and it evaluates to the thing that we did not want."""
+    def __init__(self, statement: str, value1,
+                 value2, formattingStatement: str,
+                 assertionName: str, description: str):
+        """the statement is the statement in the function for checking the values
+        value 1 and value2 are the values of assertion
+        formattingStatement is the statement for formatting the values to show the condition
+        assertionName is the name of assertion that occurs
+        description is the message about what have happened"""
+        message = ""
+        message += statement+'\n'
+        message += formattingStatement.format(str(value1, str(value2)))+'\n'
+        message += assertionName+":"+description+'\n'
+
+        super().__init__(message)
+
+
+
+
+
+
+
