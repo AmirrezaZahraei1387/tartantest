9ec78ee7edcc544bc81b1f6a85f901915739e2eb
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-07-16 13:22:23 -0600

    a docstring is provided for asserts

---
+++ asserts/asserts.py
@@ -0,0 +1,4 @@
+"""in this code file we implement different forms of
+assertions for the tests that are written. they are implemented
+as functions and not classes, and they can raise an error
+when they are not satisfied with the given input."""
