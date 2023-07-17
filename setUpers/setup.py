9e103001baaa491c28da027bcb08ee2bf7443271
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-07-17 14:30:32 -0600

    the runMethod in class SetDown is created

--- setUpers/setup.py
+++ setUpers/setup.py
@@ -21,6 +21,8 @@ import setUpers.errors as errors
 class SetDown:
 
     names: list = {"setup": "setup", "takedown": "takedown"}
+    SEARCH_NAMES = 0    # this is the value for searching among names
+    SEARCH_ADDrESS = 1  # this is a value that determine search among addresses
 
     def __init__(self, allMethods):
         """these are the names of all  methods of class
@@ -32,10 +34,17 @@ class SetDown:
             if method[0].startswith(self.names["setup"]) or method[0].startwith(self.names["takedown"]):
                 self.__setDownMethods.append(method)
 
-    def search(self, item, mode):
+    def search(self, item, mode: int):
         """here item is something that we want to search in the list
         and the mode determines, where to search because the __SetDownMethods
         contain a tuple and each tuple have the name and address."""
+        index = -1
+
+        for method in self.__setDownMethods:
+            index += 1
+            if method[mode] == item:
+                return index
+        raise IndexError("item is not found")
 
 
     def runMethod(self, nameMethod):
@@ -44,12 +53,12 @@ class SetDown:
         takedownMethod = self.names["takedown"]+str(nameMethod)
 
         try:
-            self.__setDownMethods.index(setupMethod)
+            setupMethodIndex = self.search(setupMethod, self.SEARCH_NAMES)
         except IndexError:
             setupMethod = False
 
         try:
-            self.__setDownMethods.index(setupMethod)
+            takedownMethodIndex = self.search(takedownMethod, self.SEARCH_NAMES)
         except IndexError:
             takedownMethod = False
 
@@ -60,8 +69,11 @@ class SetDown:
             raise errors.BothSetupTakedownError("expected to provide both setup and takedown method/function")
 
         else:
-            return {}
+            return {"setup":self.__setDownMethods[setupMethodIndex], "takedowen":self.__setDownMethods[takedownMethodIndex]}
 
+
+ 
+        
 
 
 
