345798b6a1f9202ac045a51d7c0b6aa4e4130b05
amirrezazahraei <amirreza.zahraei1@rbe.sk.ca>
2023-07-17 14:49:14 -0600

    the __init__ file now has the Base class to import it

--- testbase/__init__.py
+++ testbase/__init__.py
@@ -3,4 +3,4 @@ import testbase.errors as errors
 from testbase.testbclass import TestBClass
 from testbase.testbfunc import TestBFunc
 import testbase.testall as testall
-
+from testbase.testbclass import Base
