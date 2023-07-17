d72fa4fae97140ab74ec8319ee3fae458d19492b
Amirreza <amirreza.zahraei1@rbe.sk.ca>
2023-07-17 11:40:50 -0600

    the __init__ is created and now runMethod is on way

--- setUpers/setup.py
+++ setUpers/setup.py
@@ -14,16 +14,84 @@ takedownA
 note for every setup a takedown is needed respectively
 """
 
+# here methods refer to both function and method
+import setUpers.errors as errors
+
 
 class SetDown:
 
-    names:list = ["setup, takedown"]
+    names: list = {"setup": "setup", "takedown": "takedown"}
 
     def __init__(self, allMethods):
         """these are the names of all  methods of class
         or functions containing both tests and other functions and methods"""
 
+        self.__setDownMethods = []
+
         for method in allMethods:
-            if method[0].startswith(self.names[0]):
+            if method[0].startswith(self.names["setup"]) or method[0].startwith(self.names["takedown"]):
+                self.__setDownMethods.append(method)
+
+    def runMethod(self, nameMethod):
+
+        setupMethod = self.names["setup"]+str(nameMethod)
+        takedownMethod = self.names["takedown"]+str(nameMethod)
+
+        try:
+            self.__setDownMethods.index(setupMethod)
+        except IndexError:
+            setupMethod = False
+
+        try:
+            self.__setDownMethods.index(setupMethod)
+        except IndexError:
+            takedownMethod = False
+
+        if setupMethod is False and takedownMethod is False:
+            return 0
+
+        elif setupMethod != takedownMethod:
+            raise errors.BothSetupTakedownError("expected to provide both setup and takedown method/function")
+
+        else:
+            return {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
